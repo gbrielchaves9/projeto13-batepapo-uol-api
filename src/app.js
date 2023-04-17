89aa59bd296655f93ebf7267ef3a224966465ac3
Gabriel Chaves <gbrielchaves9@gmail.com>
2023-04-16 21:46:38 -0300

    cm1

--- src/app.js
+++ src/app.js
@@ -1,10 +1,27 @@
-import express from "express";
-import cors from 'cors'
+import express from "express"
+import cors from "cors"
+import { MongoClient } from "mongodb"
+import dotenv from "dotenv"
 
 const app = express()
-app.use(cors())
+
+
 app.use(express.json())
+app.use(cors())
+dotenv.config()
 
 
+let db
+const mongoClient = new MongoClient(process.env.DATABASE_URL)
+mongoClient.connect()
+    .then(() => db = mongoClient.db())
+    .catch((err) => console.log(err.message))
+
+
+/*app.get("/", (req,res)=>{
+    db.collection("herois").find().toArray()
+    .then(herois=> res.send(herois))
+    .catch(err = res.status(500).send(err.message))
+})*/
 const PORT = 5000
-app.listen(PORT)
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
